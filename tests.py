13038c9cb56c8ff133da1e279b6e61ded8cb65ac
jslgmy <49324049+jslgmy@users.noreply.github.com>
2020-03-01 22:52:58 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,21 +2,23 @@ import unittest
 import math
 import task
 
+
 class TestCase(unittest.TestCase):
 
-  def test1(self):
-    expected = "success"
-    self.assertEqual(expected, task.firstrun())
-    
-  def test2(self):
-    expected = "failure"
-    self.assertNotEqual(expected, task.firstrun())
-    
-  def test3(self):
-    radius = 1
-    expected = [1, 5]
-    self.assertEqual(expected, task.circle_area(radius))
-    
-   
+    def test1(self):
+        expected = "success"
+        self.assertEqual(expected, task.firstrun())
+
+    def test2(self):
+        expected = "failure"
+        self.assertNotEqual(expected, task.firstrun())
+
+    def test3(self):
+        radius = 1
+        expected = [1, 5]
+        self.assertEqual(expected, task.circle_area(radius))
+
+
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
+
