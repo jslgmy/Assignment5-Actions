c8f31bea790ed7233a4429da31ce81df31624b79
jslgmy <49324049+jslgmy@users.noreply.github.com>
2020-03-01 22:50:38 -0800

    Update task.py

--- task.py
+++ task.py
@@ -1,9 +1,11 @@
 import math
 
+
 def firstrun():
-  return "success"
+    return "success"
+
 
 def circle_are(radius):
-  area = (radius * radius)
-  area = math.pi * area
-  return area
+    area = (radius * radius)
+    area = math.pi * area
+    return area
